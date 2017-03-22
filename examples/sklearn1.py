5b181101faec231e4590ef95305e0dc774aef44b
William Doran <wfdoran4@yahoo.com>
2017-03-22 15:21:10 -0700

    Improved sklearn1.py.  Train on halp, test on other half.

--- examples/sklearn1.py
+++ examples/sklearn1.py
@@ -18,3 +18,14 @@ plt.plot([y.min(), y.max()], [y.min(), y.max()], color='black')
 plt.xlabel('Actual')
 plt.ylabel('Model')
 plt.show()
+
+# train on half of the data, test on the other half
+n = len(X) // 2
+model.fit(X[:n], y[:n])
+z = model.predict(X[n:])
+
+plt.scatter(y[n:],z, color='blue')
+plt.plot([y.min(), y.max()], [y.min(), y.max()], color='black')
+plt.xlabel('Actual')
+plt.ylabel('Model')
+plt.show()
