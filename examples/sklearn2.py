051ac3d9509e96fdc6776a43e60ab4b92d67b8d6
William Doran <wfdoran4@yahoo.com>
2017-03-23 18:49:50 -0700

    Rework sklearn2.py to show each of the 10 classes.

--- examples/sklearn2.py
+++ examples/sklearn2.py
@@ -11,6 +11,7 @@ pca = decomposition.PCA(n_components=2)
 X_features = pca.fit(X,None).transform(X)
 
 # print the features correpsonding to 0
+icon = ['bo', 'go', 'bs', 'co', 'mo', 'ro', 'gs', 'rs', 'cs', 'ms']
 for i in range(len(X_features)):
-    plt.plot(X_features[i,0], X_features[i,1], 'ro' if y[i] == 0 else 'gs')
+    plt.plot(X_features[i,0], X_features[i,1], icon[y[i]])
 plt.show()
